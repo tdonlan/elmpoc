b5a01379ecb16d50ce6b85ef7cf6b636d2e93fec
Tim <timdonlan@gmail.com>
2017-04-08 21:35:22 -0400

    fixed elm packaging, main.elm in index.html

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("frontend"))
-	http.Handle("/", fs)
+	r := mux.NewRouter()
+	r.HandleFunc("/api/hello", helloHandler)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("frontend")))
+
+	http.Handle("/", r)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	//vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello World!")
 }
