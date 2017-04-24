7fa8a5c1a2d86712571e4012b469c946e4b7a6b5
Tim Donlan <tim.donlan@patientco.com>
2017-04-23 21:53:25 -0400

    elm tutorial, fetch json from GET route

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 task :build do
 	sh "go build elmpoc"
-	sh "cd elm && elm-make examples/main.elm --output ../frontend/js/main.js && cd .."
+	sh "cd elm && elm-make src/Main.elm --output ../frontend/js/main.js && cd .."
 end
 task :run => :build do
 	sh "./elmpoc"
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -9,6 +10,7 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/players", getPlayersHandler)
 	r.HandleFunc("/api/hello", helloHandler)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("frontend")))
 
@@ -19,7 +21,33 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
+
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello World! %#v", r)
+}
+
+type Player struct {
+	Id    string    `json:"id"`
+	Name  string `json:"name"`
+	Level int    `json:"level"`
+}
+
+func getPlayersHandler(w http.ResponseWriter, r *http.Request) {
+	players := []Player{}
+
+	players = append(players, Player{"1", "Ginny", 1})
+	players = append(players, Player{"2", "Tim", 35})
+	players = append(players, Player{"3", "Jack", 3})
+	players = append(players, Player{"4", "Porter", 6})
+	players = append(players, Player{"5", "Steph", 35})
+
+	playersJson, err := json.Marshal(players)
+	if err != nil {
+		w.WriteHeader(500)
+
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(playersJson)
 }
